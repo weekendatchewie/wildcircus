a660450b04e2280f15951a71b4988c37ab3b1349
Christopher Poncelet <christopher.poncelet51@gmail.com>
2020-01-29 14:30:30 +0100

    page home

--- wildcircus/src/components/Home.js
+++ wildcircus/src/components/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import './Home.css';
 
 class Home extends React.Component {
   render() {
     return(
       <div className="home">
         <div className="cardHome">
-          <img src="" alt="" />
+          <img className="imgHome" src="https://images.pexels.com/photos/2337777/pexels-photo-2337777.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="image circus" />
+          <h1 className="titleHome">Welcome to The Wild Circus</h1>
         </div>
+        <button>Come & See</button>
       </div>
     )
   }
 }
+
+export default Home;
