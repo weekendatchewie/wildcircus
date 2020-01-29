e24bc98129e0b50534ea988b329f6473976c0d38
Christopher Poncelet <christopher.poncelet51@gmail.com>
2020-01-29 11:47:16 +0100

    install router create first page

---
+++ wildcircus/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+class Home extends React.Component {
+  render() {
+    return(
+      <div className="home">
+        <div className="cardHome">
+          <img src="" alt="" />
+        </div>
+      </div>
+    )
+  }
+}
